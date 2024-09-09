f04a18dcc98292e50ac9db4fd2b6f5e36256975c
Igor Assaife <igorassaife@gmail.com>
2024-09-09 21:58:37 +0000

    fixing pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return <h1> Gacha de papelao destruindo vidas... e carteiras
-    <p><img src="https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546"> </p>
+    <p><img src="https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546" /> </p>
 
     </h1>         
          
