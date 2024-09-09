4c39e7fb6e1529252a4d2578a62c3ea8efe6af88
Igor Assaife <igorassaife@gmail.com>
2024-09-09 21:52:45 +0000

    adding memes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1> Gacha de papelao destruindo vidas... e carteiras :pepehands: </h1>
+    return <h1> Gacha de papelao destruindo vidas... e carteiras
+        <img src="https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546"></img>
+         </h1>
+         
 }
 
 export default Home;
