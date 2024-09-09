ae60d320c8d1d1fd4ebbd37f1e24111ad79f89d8
Igor Assaife <igorassaife@gmail.com>
2024-09-09 22:00:46 +0000

    fix 2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
     return <h1> Gacha de papelao destruindo vidas... e carteiras
-    <p><img src="https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546" /> </p>
+    <p>
+        <div class="tenor-gif-embed" data-postid="12683546" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546">Pepe Why Pepehands GIF</a>from <a href="https://tenor.com/search/pepe+why-gifs">Pepe Why GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script> </p>
 
     </h1>         
          
