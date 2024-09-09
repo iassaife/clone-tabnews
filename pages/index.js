49f49422b6e8f58599dee8282a84311eda9234e0
Igor Assaife <igorassaife@gmail.com>
2024-09-09 21:57:37 +0000

    more memes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return <h1> Gacha de papelao destruindo vidas... e carteiras
-        <img src="https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546"></img>
-         </h1>
+    <p><img src="https://tenor.com/view/pepe-why-pepe-pepehands-crying-gif-12683546"> </p>
+
+    </h1>         
          
 }
 
