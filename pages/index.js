6f60bb1941ead559fba80acc68804da5c6ba9878
Igor Assaife <igorassaife@gmail.com>
2024-09-12 21:39:34 +0000

    adding `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
-    return <h1> Gacha de papelao destruindo vidas... e carteiras
-    <p>
-        <img src= "https://media1.tenor.com/m/Nu77sMjXsX0AAAAC/pepe-why-pepe.gif" />
-    </p>   
-
-    </h1>                  
+  return (
+    <h1>
+      {" "}
+      Gacha de papelao destruindo vidas... e carteiras
+      <p>
+        <img src="https://media1.tenor.com/m/Nu77sMjXsX0AAAAC/pepe-why-pepe.gif" />
+      </p>
+    </h1>
+  );
 }
 
 export default Home;
